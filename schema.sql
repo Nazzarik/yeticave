e34138ba5239b0db9d7627e049ec833a9c5d61dc
Nazzarik <nazzarik@gmail.com>
2018-05-27 01:21:24 +0300

    chenged error and finish task1

--- schema.sql
+++ schema.sql
@@ -1,9 +1,34 @@
+drop database if exists yeticave;
+
 create database yeticave
-default character set utf8
-default collate utf8_general_ci;
+  default character set utf8
+  default collate utf8_general_ci;
 
 use yeticave;
 
-create table user (
-id int auto_increment
-)
+create table categories (
+  id tinyint unsigned auto_increment primary key,
+  name char(50) not null
+);
+
+create table users (
+  id int auto_increment primary key,
+  name char(128),
+  email char(128),
+  password_hash char(255),
+  avatar char(255)
+);
+
+create table lots (
+  id  int auto_increment primary key,
+  name char(255),
+  category_id tinyint,
+  image char(128),
+  lote_rate int(12),
+  lot_step int(12),
+  close_date datetime,
+  description char(255)
+);
+
+create unique index name on users(name);
+create unique index email on users(email);
