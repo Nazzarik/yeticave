05c525b73880b82ceb6f2d7bbdebaf14385bd54d
Nazzarik <nazzarik@gmail.com>
2018-05-30 22:19:18 +0300

    add logout

--- templates/header-common.php
+++ templates/header-common.php
@@ -17,7 +17,7 @@
             </div>
             <div class="user-menu__logged">
                 <p><?=$user['name'];?></p>
-                <a href="#">Выйти</a>
+                <a href="logout.php">Выйти</a>
             </div>
 
         <?php else: ?>
